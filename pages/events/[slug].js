351783443531338837e8a36f8fc89184734babcb
Max Klopsch <m.p.klopsch@gmail.com>
2021-09-21 19:47:54 +0100

    Add a map to event pages

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -2,6 +2,7 @@ import { FaPencilAlt, FaTimes } from 'react-icons/fa';
 import Link from 'next/link';
 import Image from 'next/image';
 import Layout from '@/components/Layout';
+import EventMap from '@/components/EventMap';
 import { API_URL } from '@/config/index';
 import { useRouter } from 'next/router';
 import styles from '@/styles/Event.module.css';
@@ -32,6 +33,8 @@ export default function EventPage({ evt }) {
         <p>{evt.description}</p>
         <h3>Venue: {evt.venue}</h3>
         <p>{evt.address}</p>
+
+        <EventMap evt={evt} />
 
         <Link href='/events'>
           <a className={styles.back}>{'<'} Go Back</a>
